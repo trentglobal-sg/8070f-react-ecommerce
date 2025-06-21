2ddef615a0d91f9935acec2ebbce0e83788b0cf6
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-06-21 07:46:04 +0000

    Added list rendering

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,37 +1,53 @@
 import Header from "./Header";
 import ProductCard from "./ProductCard";
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+
+
+    const [featuredProducts, setFeaturedProducts] = useState([]);
+
+    // useEffect: an effect in React is a logic not related to rendering the DOM
+    // first parameter of useEffect is a 'effect' function
+    // second parameter is a dependency array - usually contains a state. When the state changes, the
+    // 'effect' function will run again.
+    //  - If the dependency array is empty, it means the effect will only run when the component renders
+    //    for the first time (aka. mounting)
+    useEffect(function () {
+
+
+
+        // make an axios call to featured.json (looks for JSON file in the public folder)
+        // the effect function cannot be aysnc 
+        const getFeaturedProducts = async () => {
+            const response = await axios.get('featured.json');
+            setFeaturedProducts(response.data);  // <-- cause a re-render
+        }
+        getFeaturedProducts();
+
+    }, [])
+
+
     return (<>
         <Header />
 
         <main className="container my-5">
             <h2 className="text-center mb-4">Featured Products</h2>
             <div className="row">
+                
+            {
+                featuredProducts.map(p=>
                 <div className="col-md-3 mb-4">
-                    <ProductCard imageUrl="https://picsum.photos/id/20/300/200"
-                        productName="Product 1"
-                        price={19.99}
+                    <ProductCard
+                        imageUrl={p.image}
+                        productName={p.name}
+                        price={p.price}
                     />
                 </div>
-                <div className="col-md-3 mb-4">
-                    <ProductCard imageUrl="https://picsum.photos/id/1/300/200"
-                        productName="Product 2"
-                        price={29.99}
-                    />
-                </div>
-                <div className="col-md-3 mb-4">
-                    <ProductCard imageUrl="https://picsum.photos/id/26/300/200"
-                        productName="Product 3"
-                        price={39.99}
-                    />
-                </div>
-                <div className="col-md-3 mb-4">
-                    <ProductCard imageUrl="https://picsum.photos/id/96/300/200"
-                        productName="Product 4"
-                        price={49.99}
-                    />
-                </div>
+                )
+            }
+             
             </div>
         </main>
 
--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,7 +1,46 @@
+
+import { useEffect, useState } from "react"
+import axios from "axios"
+import ProductCard from "./ProductCard";
+
 export default function ProductPage() {
+
+    // store the products in a state
+    const [products, setProducts] = useState([]);
+
+    useEffect( () => {
+
+        const fetchProducts = async () => {
+           const response =  await axios.get('products.json');
+           setProducts(response.data);
+           
+        }
+        fetchProducts();
+
+
+    }, []); // <-- empty array means run once when the component is mounted (aka, rendered for the first time)
+
+
+    const renderProducts = () => {
+        const productJSX = [];
+        for(let p of products) {
+            productJSX.push(<div key={p.id} className="col-md-4 mb-4">
+                <ProductCard
+                    imageUrl={p.image}
+                    price={p.price}
+                    name={p.name}
+                />
+            </div>)
+        }
+        return productJSX;
+    }
+
     return (<>
         <div className="container mt-5">
             <h1>Our Products</h1>
+            <div className="row">
+            {renderProducts()}
+            </div>
         </div>
     </>)
 }
