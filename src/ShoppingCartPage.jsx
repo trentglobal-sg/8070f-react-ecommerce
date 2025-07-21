ed5cf004886d00eeb5e028e817ffd8431d3a59ee
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-07-21 11:54:19 +0000

    Added checkout

--- src/ShoppingCartPage.jsx
+++ src/ShoppingCartPage.jsx
@@ -1,18 +1,32 @@
+import axios from "axios";
 import { useCart } from "./CartStore";
 import { useEffect } from "react";
+import { useJwt } from "./UserStore";
 
 
 export default function ShoppingCartPage() {
 
     const { cart, modifyQuantity, removeFromCart, fetchCart } = useCart();
+    const { getJwt } = useJwt();
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchCart();
     }, []);
+
+    const checkout = async () => {
+        const jwt = getJwt();
+        const response = await axios.post(import.meta.env.VITE_API_URL + '/api/checkout', {}, {
+            headers: {
+                Authorization: "Bearer " + jwt
+            }
+        });
+        window.location = response.data.url;
+    }
 
     return (<>
         <div className="container">
             <h1>Shopping Cart</h1>
+            <button class="btn btn-primary mt-3 mb-3" onClick={checkout}>Checkout</button>
             <ul className="list-group mb-3">
                 {
 
@@ -22,16 +36,16 @@ export default function ShoppingCartPage() {
                                 <h5>{item.product_name}</h5>
                                 <div className="d-flex align-items-center">
                                     <button className="btn btn-primary btn-sm"
-                                            onClick={()=>modifyQuantity(item.product_id, item.quantity - 1)}
-                                            disabled={item.quantity === 1}
+                                        onClick={() => modifyQuantity(item.product_id, item.quantity - 1)}
+                                        disabled={item.quantity === 1}
                                     >-</button>
                                     <p className="mb-0 ms-1 me-1">Quantity: {item.quantity}</p>
                                     <button className="btn btn-primary btn-sm"
-                                            onClick={()=>modifyQuantity(item.product_id, item.quantity + 1)}
+                                        onClick={() => modifyQuantity(item.product_id, item.quantity + 1)}
                                     >+</button>
                                     <div>
                                         <button className="btn btn-danger btn-sm ms-1"
-                                            onClick={()=>{
+                                            onClick={() => {
                                                 removeFromCart(item.product_id)
                                             }}
                                         >
